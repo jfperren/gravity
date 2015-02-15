25a05556fd14d8dbf3e6918807a01fbda3358e1d
Julien Perrenoud <julien.perrenoud@epfl.ch>
2015-02-15 18:06:58 +0100

    Clean Explosion

--- Gravity [Reworked]/src/ch/chamblandes/gravity/displayables/Explosion.java
+++ Gravity [Reworked]/src/ch/chamblandes/gravity/displayables/Explosion.java
@@ -1,11 +1,15 @@
 package ch.chamblandes.gravity.displayables;
 
+import static ch.chamblandes.gravity.model.GameEngine.UNIT;
+
 import java.awt.Color;
 import java.awt.Graphics;
 
-import ch.chamblandes.gravity.model.GravityApplet;
-
 public class Explosion extends CircularDisplayable {
+
+    public static final int MAX_RADIUS = 15 * UNIT;
+    public static final int MIDDLE_RADIUS = 8 * UNIT;
+    public static final int MIN_RADIUS = 0;
 
     Color couleur;
     boolean isGrowing;
@@ -13,20 +17,20 @@ public class Explosion extends CircularDisplayable {
     public Explosion(double positionX, double positionY, double rayon, Color couleur) {
         super(positionX, positionY, rayon);
         this.couleur = couleur;
-        isGrowing = true;
+        this.isGrowing = true;
     }
 
     public void explode() {
 
-        if (isGrowing) {
-            if (this.getRadius() > (GravityApplet.PANEL_WIDTH / 25)) {
-                isGrowing = false;
+        if (this.isGrowing) {
+            if (this.getRadius() > (MAX_RADIUS)) {
+                this.isGrowing = false;
             } else {
                 this.setRadius(this.getRadius() + (this.getRadius() / 5));
             }
 
         } else {
-            if (this.getRadius() > (GravityApplet.PANEL_WIDTH / 50)) {
+            if (this.getRadius() > (MIDDLE_RADIUS)) {
                 this.setRadius(this.getRadius() - (this.getRadius() / 30));
             } else if (this.getRadius() > 0) {
                 this.setRadius(this.getRadius() - (this.getRadius() / 10));
@@ -36,7 +40,7 @@ public class Explosion extends CircularDisplayable {
 
     @Override
     public void paint(Graphics g) {
-        g.setColor(couleur);
+        g.setColor(this.couleur);
         g.fillOval((int) (this.getX() - this.getRadius()), (int) (this.getY() - this.getRadius()),
             (int) (2 * this.getRadius()), (int) (2 * this.getRadius()));
     }
